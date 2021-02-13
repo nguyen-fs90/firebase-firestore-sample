7ff7f50fde890e603c2dbe628cb2389304ccfcbc
Nguyen Manh Hiep <nguyen.fs90@gmail.com>
2021-02-13 22:38:29 +0900

    todo app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import {useEffect, useState} from 'react';
 import './App.css';
+// import {db} from './Firebase';
 
 function App() {
+  const [task, setTask] = useState('');
+  const [todoList, setTodoList] = useState([]);
+
+  useEffect(() => {
+    console.log('is effect !');
+  }, []);
+
+  const onChange = (e) => {
+    setTask(e.target.value);
+  }
+
+  const addTodo = () => {
+    setTodoList([...todoList, task]);
+    setTask('');
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todo-app">
+      <div className="header">
+        <h3>Todo List</h3>
+      </div>
+      <div className="container">
+        <input type="text" onChange={onChange} value={task}></input>
+        <button type="button" onClick={addTodo}>add</button>
+        <ul>
+          {todoList.map((todo) => <li key={todo}>{todo}</li>)}
+        </ul>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import firebase from './Firebase';
 
 ReactDOM.render(
   <React.StrictMode>
